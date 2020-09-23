09ca5cf2af89c0bca3e41615b3dcd9e06d47ab11
Umesh K Jois <umeshk7886@gmail.com>
2020-09-23 21:19:48 +0530

    Lesson #4 of Russ's Svelte Tutorial

--- src/components/PersonList.svelte
+++ src/components/PersonList.svelte
@@ -2,6 +2,13 @@
     import Person from './Person.svelte';
     import StarWarsStore from '../store/StarWars';
     let allPersons = StarWarsStore.getAllPersons();
+    function loadList({ target }){
+        if(target.dataset.link != null){
+            allPersons = StarWarsStore.getByURL(target.dataset.link);
+        }
+    }
+
+
 </script>
 
 <div class="personList">
@@ -18,6 +25,12 @@
         {#each persons.results as person}
         <Person {person}/>
     {/each}
+    <div class="buttons">
+        <button data-link="{persons.previous}" disabled="{(persons.previous) ? '' : 'true'}" on:click={loadList}>Previous 10</button>
+        <button data-link="{persons.next}" disabled="{(persons.next) ? '' : 'true'}" on:click={loadList}>Next 10</button>
+    </div>
+    
+    
     {:catch error}
         <p>Whoops, there was an error! {JSON.stringify(error)} </p>
     {/await}
@@ -52,4 +65,29 @@
   0% { transform: rotate(0deg); }
   100% { transform: rotate(360deg); }
 }
+
+.buttons {
+        width: 100%;
+        text-align: center;
+        
+    }
+    .buttons button {
+        padding: 10px 20px;
+        border-radius: 10px;
+        border: none;
+        outline: none;
+        margin: 5px;
+        background-color: #f3ef08;
+        color: rgb(93, 7, 253);
+        font-size: 1.5em;
+        cursor: pointer;
+    }
+
+    button:disabled {
+    background-color: #f3ef08;
+    color: #999;
+    cursor: none;
+}
+
+
 </style>
