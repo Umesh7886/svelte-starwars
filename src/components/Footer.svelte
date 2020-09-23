954d960aef29611b3b400ec4764bb8dabe67bb8d
Umesh K Jois <umeshk7886@gmail.com>
2020-09-23 12:24:31 +0530

    Lesson #1 of Russ's Svelte Tutorial

---
+++ src/components/Footer.svelte
@@ -0,0 +1,20 @@
+<footer class="footer">
+    <p>Umesh K Jois &copy; 2020</p>
+    <p>Star Wars information provided by <a href="https://swapi.dev/">The Star Wars API</a></p>
+</footer>
+
+<style>
+    .footer {
+        padding: 5px;
+        border-top: 1px solid #333;
+        text-align: center;
+        background-color: var(--backgroundColor);
+        color: #fff;
+        font-family: 'Rowdies', cursive;
+    }
+
+    a {
+        color: #FFE300;
+    }
+</style>
+
---
+++ src/components/Header.svelte
@@ -0,0 +1,28 @@
+<header class="header">
+    <h1>Star Wars Information</h1>
+    <h2>Provided by <span>The Star Wars API</span></h2>
+</header>
+
+<style>
+    .header {
+        height: 100px;
+        width: 100%;
+        background-color: var(--backgroundColor);
+        color: aliceblue;
+        margin: 0;
+        padding: 10px;
+        text-align: center;
+        font-family: 'Rowdies', cursive;
+    }
+    h1 {
+        margin-top: 0;
+    }
+    h2 {
+        margin-bottom: 0;
+    }
+
+    h2 > span {
+        color: #FFE300;
+    }
+</style>
+
